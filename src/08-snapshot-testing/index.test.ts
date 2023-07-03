48b5abdae9cc1418b3e5d5c0adbd9fef28e3b234
Miras <miamib531@gmail.com>
2023-07-03 14:05:31 +0600

    feat: write unit tests for 08-snapshot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,27 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const elements = [0, 1, 2];
+    const expectedLinkedList = {
+      value: 0,
+      next: {
+        value: 1,
+        next: {
+          value: 2,
+          next: { value: null, next: null },
+        },
+      },
+    };
+    const result = generateLinkedList(elements);
+    expect(result).toStrictEqual(expectedLinkedList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const elements = [4, 5, 6];
+    const result = generateLinkedList(elements);
+    expect(result).toMatchSnapshot();
   });
 });
